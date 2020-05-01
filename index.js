96b107c51778e33e5b171c24ba585eb34a8bf530
André Felizardo <andre.luis.felizardo@accenture.com>
2020-05-01 12:23:26 -0300

    Adicionando index e finalizando interface de upload de imagem

---
+++ index.js
@@ -0,0 +1,49 @@
+const AWS = require('aws-sdk');
+
+
+const S3 = new AWS.S3();
+const Rekognition = new AWS.Rekognition();
+
+const bucketParams = {
+  Bucket: 'face-analise-js-example',
+};
+
+function listaImagens() {
+  let imagens = [];
+
+  S3.listObjects(bucketParams, (erro, data) => {
+    if (erro) {
+      console.log(erro);
+    } else {
+      const arrayImagens = data.Contents;
+
+      arrayImagens.forEach((imagem) => {
+        imagens.push(imagem.Key);
+      });
+
+      indexaColecao(imagens)
+    }
+  });
+}
+
+function indexaColecao(imagens) {
+  imagens.forEach((imagem) => {
+      console.log(imagem);
+    Rekognition.indexFaces({
+      CollectionId: 'faces',
+      DetectionAttributes: [],
+      ExternalImageId: imagem.slice(0, imagem.length - 4),
+      Image: {
+        S3Object: {
+          Bucket: 'face-analise-js-example',
+          Name: imagem,
+        },
+      },
+    }, (erro, data) => {
+        if (erro) console.log(erro, erro.stack)
+        else console.log(data)
+    });
+  });
+}
+
+// listaImagens();
