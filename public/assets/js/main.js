815b31616cc969b60ea9fefbcea0a60968ec2833
André Felizardo <andre.luis.felizardo@accenture.com>
2020-05-05 08:05:18 -0300

    feat: Fetch e desenhos arquitetura.
    Enviando imagem para API Gateway através de fetch.
    Adição dos arquivos de desenho da arquitetura.

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,10 +1,32 @@
 function actionChooseImage() {
-    const input = document.querySelector('input[type=file]#image');
-    input.addEventListener('change', (data) => {
-        // console.log(data);
-        const file = data.path[0].files[0];
-        console.log(file)
-    })
+  const input = document.querySelector('input[type=file]#image');
+  input.addEventListener('change', (data) => {
+    const file = data.path[0].files[0];
+    const blob = file.slice(0, file.size, file.type);
+    const newName = file.name.replace(
+      file.name.slice(0, file.name.indexOf('.')),
+      '_analise'
+    );
+    newFile = new File([blob], newName, { type: file.type });
+    sendImageToBucket(newFile);
+  });
+}
+
+function sendImageToBucket(file) {
+    const headersAPI = new Headers();
+    headersAPI.append('x-api-key', 'KVeA4Rn3qPaOm0TOIw7lT44F2taCi9q63fn99GP2');
+    fetch(
+      'https://4vic25fj6b.execute-api.us-east-1.amazonaws.com/prod/upload-to-s3',
+      {
+        method: 'POST',
+        body: file,
+        headers: headersAPI,
+      }
+    )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => console.log(error));
 }
 
 actionChooseImage();
