6f58d2e6a6dec951ccd9c14a6b68a650737b810e
André Felizardo <andre.luis.felizardo@accenture.com>
2020-05-21 09:52:02 -0300

    refactor(Frontend): Mudando  fetch para axios

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,39 +2,43 @@ function actionChooseImage() {
   const input = document.querySelector('input[type=file]#image');
   input.addEventListener('change', (data) => {
     const file = data.path[0].files[0];
+    console.log(file);
     const blob = file.slice(0, file.size, file.type);
     const newName = file.name.replace(
       file.name.slice(0, file.name.indexOf('.')),
       '_analise'
     );
     newFile = new File([blob], newName, { type: file.type });
-    sendImageToBucket(newFile);
+    sendImageToBucket(newFile, file.type);
   });
 }
 
 function initUpload() {
-  document.querySelector('.container-principal__botao-upload').classList.add('uploading')
+  document
+    .querySelector('.container-principal__botao-upload')
+    .classList.add('uploading');
 }
 
-function sendImageToBucket(file) {
-    initUpload();
-    const headersAPI = new Headers();
-    headersAPI.append('x-api-key', 'KVeA4Rn3qPaOm0TOIw7lT44F2taCi9q63fn99GP2');
-    fetch(
+function sendImageToBucket(file, fileType) {
+  initUpload();
+  axios
+    .post(
       'https://4vic25fj6b.execute-api.us-east-1.amazonaws.com/prod/upload-photo',
+      file,
       {
-        method: 'POST',
-        body: file,
-        headers: headersAPI,
+        headers: {
+          'x-api-key': 'KVeA4Rn3qPaOm0TOIw7lT44F2taCi9q63fn99GP2',
+          'content-type': fileType
+        },
       }
     )
-      .then((res) => {
-        console.log(res);
-      })
-      .catch((error) => {
-        alert('Provavelmente erro de CORS filhão')
-        console.log(error)
-      });
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      alert('Provavelmente erro de CORS filhão');
+      console.log(error);
+    });
 }
 
 actionChooseImage();
